c95c315d00c0aa4527113391979f7a0933545efb
pr0m1th3as <abertsatos@biol.uoa.gr>
2025-02-01 17:17:29 +0200

    add docstring in PolarAreaData classdef

--- inst/PolarAreaData.m
+++ inst/PolarAreaData.m
@@ -16,6 +16,89 @@
 ## this program; if not, see <http://www.gnu.org/licenses/>.
 
 classdef PolarAreaData
+## -*- texinfo -*-
+## @deftypefn  {chartjs} {@var{obj} =} PolarAreaData (@var{data})
+##
+## Create a @qcode{PolarAreaData} object.
+##
+## @code{@var{obj} = PolarAreaData (@var{data})} returns a @qcode{PolarAreaData}
+## object, in which @var{data} must be a nonempty numerical vector containing
+## the radii of the arc segments of a single dataset.  Constructing a
+## @qcode{PolarAreaData} object always assigns the default property values,
+## which can later be modified using dot notation syntax.
+##
+## A @qcode{PolarAreaData} object, @var{obj}, contains the following properties:
+##
+## @multitable @columnfractions 0.23 0.02 0.75
+## @headitem @var{Field} @tab @tab @var{Description}
+##
+## @item @qcode{backgroundColor} @tab @tab A @qcode{Color} object defining the
+## color of the face of the arc segments.  Default is empty, in which case and a
+## color is assigned automatically by the Chart.js library.
+##
+## @item @qcode{borderAlign} @tab @tab A character vector, which can be either
+## @qcode{'center'} (default) or @qcode{'inner'}.  When @qcode{'center'} is set,
+## the borders of arcs next to each other will overlap.  When @qcode{'inner'} is
+## set, it is guaranteed that all borders will not overlap.
+##
+## @item @qcode{borderColor} @tab @tab A @qcode{Color} object defining the color
+## of the border line of the arc segments.  Default is empty, in which case a
+## color is assigned automatically by the Chart.js library.
+##
+## @item @qcode{borderDash} @tab @tab A numeric vector defining the length and
+## spacing of dashes drawn at the borders of the arc segments.
+##
+## @item @qcode{borderDashOffset} @tab @tab A numeric scalar defining the offset
+## of dashes drawn at the borders of the arc segments.  Default is 0.
+##
+## @item @qcode{borderJoinStyle} @tab @tab A character vector defining the arc
+## border join style.  It can be either @qcode{'round'}, @qcode{'bevel'}, or
+## @qcode{'miter'}.
+##
+## @item @qcode{borderWidth} @tab @tab A numeric scalar value defining the width
+## of the each arc segment's borders in pixels.  Default is 2.
+##
+## @item @qcode{circular} @tab @tab A boolean scalar value defining whether the
+## arc will be curved or flat.  By default, it is @qcode{true} corresponding to
+## curved arc segments.
+##
+## @item @qcode{clip} @tab @tab A numeric scalar value defining the pixels to
+## clip relative to the chart's area.  Positive value allows overflow, negative
+## value clips that many pixels inside chartArea.  Defaults to zero pixels.
+##
+## @item @qcode{data} @tab @tab A numeric vector assigned at construction of the
+## @qcode{PolarAreaData} object.  It cannot be empty.
+##
+## @item @qcode{hoverBackgroundColor} @tab @tab A @qcode{Color} object defining
+## the color of the face of each arc segment, when the mouse hovers over it.
+## Default is empty, in which case the color is the same as in
+## @qcode{backgroundColor}.
+##
+## @item @qcode{hoverBorderColor} @tab @tab A @qcode{Color} object defining
+## the color of the borders of each arc segment, when the mouse hovers over it.
+## Default is empty, in which case the color is the same as in
+## @qcode{borderColor}.
+##
+## @item @qcode{hoverBorderDash} @tab @tab A numeric vector defining the length
+## and spacing of dashes drawn at the borders of the arc segment, when the
+## mouse hovers over it.
+##
+## @item @qcode{hoverBorderDashOffset} @tab @tab A numeric scalar defining the
+## offset of dashes drawn at the borders of the arc segment, when the mouse
+## hovers over it.
+##
+## @item @qcode{hoverBorderJoinStyle} @tab @tab A character vector defining the
+## arc border join style, when the mouse hovers over it.  It can be either
+## @qcode{'round'}, @qcode{'bevel'}, or @qcode{'miter'}.
+##
+## @item @qcode{hoverBorderWidth} @tab @tab A numeric scalar value defining the
+## width of the each arc segment's borders in pixels, when the mouse hovers over
+## it.
+##
+## @end multitable
+##
+## @seealso{PolarAreaChart, Color, Fill}
+## @end deftypefn
 
   properties (Access = public)
 
